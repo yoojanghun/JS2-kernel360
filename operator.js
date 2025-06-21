516726ac89dd9b4d5f5dff615828050cf70c53c8
yoojanghun <yoojanghun@naver.com>
2025-06-22 01:12:15 +0900

    operator2

--- operator.js
+++ operator.js
@@ -10,3 +10,33 @@ console.log(7 % 5);
 let a = 2;
 a += 1;
 console.log(a);
+
+
+// 비교 연산자(comparision operator)
+function isEqual(x, y) {
+    return x === y;         // data type, 값이 모두 같니?
+}
+
+function isNotEqual(x, y) {
+    return x !== y;         // data type, 값이 모두 다르니?
+}
+
+console.log(isEqual(1, 1));
+console.log(isEqual(1, '1'));
+
+console.log(isNotEqual(1, 1));
+console.log(isNotEqual(1, '1'));
+
+
+// 논리 연산자(logical operator) (&&, ||, !)
+const d = 1 === 1;              // true
+const e = 'AB' === 'AB';        // true
+const f = false                 // false
+
+console.log(d);
+console.log(e);
+console.log(f);
+
+console.log('&&: ', d && e && f);
+console.log('||: ', d || e || f);
+console.log('!: ', !d);
