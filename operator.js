a49c38306dec798d06289263af8051ea91de6de9
yoojanghun <yoojanghun@naver.com>
2025-06-22 00:59:20 +0900

    operator

---
+++ operator.js
@@ -0,0 +1,12 @@
+// 산술 연산자(arithmetic operator)
+console.log(1 + 2);
+console.log(5 - 7);
+console.log(3 * 4);
+console.log(10 / 2);
+console.log(7 % 5);
+
+
+// 할당 연산자(assignment operator) (=, +=, -=, *=, /=, %=)
+let a = 2;
+a += 1;
+console.log(a);
