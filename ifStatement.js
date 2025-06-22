32b3d097a903881d1e3f998bb212a8910868095d
yoojanghun <yoojanghun@naver.com>
2025-06-22 20:33:07 +0900

    switch statement

--- ifStatement.js
+++ ifStatement.js
@@ -13,3 +13,21 @@ if (a === 0) {
 } else {
     console.log('rest....');
 }
+
+
+// 조건문 (Switch statement)
+// 어떤 값이 딱 떨어지는 값으로 분기 처리를 한다면(a === 0, a === 2) switch문 적절.
+
+switch (a) {
+    case 0:
+        console.log('a is 0');
+        break
+    case 2:
+        console.log('a is 2');
+        break
+    case 6:
+        console.log('a is 6');
+        break
+    default:
+        console.log('rest....');
+}
